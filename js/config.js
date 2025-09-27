3dadc67b48f6c1cc8a74d2500514cbc52ac1df3a
ferencii <ferencithibor@gmail.com>
2025-09-27 11:22:36 +0200

    u

---
+++ js/config.js
@@ -0,0 +1,15 @@
+export const CONFIG = {
+    API_KEY: "TU_API_KEY",
+    STORAGE_KEY: 'gastos_empresa_proyectos',
+    EXCEL_TEMPLATE_URL: 'gastos.xlsx'
+};
+
+// filepath: c:\Users\Feren\Documents\GitHub\gastos\js\ui.js
+export function showAlert(title, message) {
+    // ... código ...
+}
+
+// filepath: c:\Users\Feren\Documents\GitHub\gastos\js\data.js
+export function saveProjects() {
+    // ... código ...
+}
